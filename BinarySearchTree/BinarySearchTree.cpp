98d8f665faf8bb5540b4211e808b421d0adb5703
LYChubby <f4uzi345@gmail.com>
2023-06-29 16:33:09 +0700

    Membuat Void Insert

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -25,4 +25,17 @@ public :
 
 		ROOT = NULL; // Initializing ROOT To NULL
 	}
+
+	void insert(string element) { // Insert a Node In The Binary Search Tree
+
+		Node* newNode = new Node(element, NULL, NULL);  // Allocate Memory For The New Node
+		newNode->info = element;  // Assign Value To The Data Field Of The New Node
+		newNode->leftchild = NULL;  // Make The Left Child Of The New Node Point To NULL
+		newNode->rightchild = NULL; // Make The Right Child Of The New Node Point To NULL
+
+		Node* parent = NULL;
+		Node* currentNode = NULL;
+		search(element, parent, currentNode);  // Locate The Node Which Will Be The Parent Of The Node To Be Inserted
+
+	}
 };
